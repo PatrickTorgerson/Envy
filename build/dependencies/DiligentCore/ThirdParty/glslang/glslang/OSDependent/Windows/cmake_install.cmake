a68a4014cc4239b583a178cfe6cb595634f2705d
PatrickTorgerson <TorgersonPatrickS@gmail.com>
2021-08-22 16:35:12 -0700

    deleting build

--- build/dependencies/DiligentCore/ThirdParty/glslang/glslang/OSDependent/Windows/cmake_install.cmake
+++ build/dependencies/DiligentCore/ThirdParty/glslang/glslang/OSDependent/Windows/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/Envy/build/install")
+  set(CMAKE_INSTALL_PREFIX "install")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
