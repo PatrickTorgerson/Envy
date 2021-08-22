3f5cd38383278e0f7253f6923691b9ce5a72799a
PatrickTorgerson <TorgersonPatrickS@gmail.com>
2021-08-22 16:33:36 -0700

    deleting

--- build/dependencies/DiligentCore/ThirdParty/SPIRV-Cross/gitversion.h
+++ build/dependencies/DiligentCore/ThirdParty/SPIRV-Cross/gitversion.h
@@ -4,10 +4,6 @@
 #ifndef SPIRV_CROSS_GIT_VERSION_H_
 #define SPIRV_CROSS_GIT_VERSION_H_
 
-<<<<<<< HEAD
-#define SPIRV_CROSS_GIT_REVISION "Git commit: 7d15e808 Timestamp: 2021-08-22T16:19:02"
-=======
-#define SPIRV_CROSS_GIT_REVISION "Git commit: 7d15e808 Timestamp: 2021-08-22T15:58:04"
->>>>>>> 3f8769199b4e5d6681c0acc5367b8bba478b1115
+#define SPIRV_CROSS_GIT_REVISION "Git commit: 7d15e808 Timestamp: 2021-08-22T16:31:54"
 
 #endif
